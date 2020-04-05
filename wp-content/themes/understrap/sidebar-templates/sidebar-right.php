909352b4f675607c0af682fd5f90056705bc53c4
Francesco Matacotti <francesco1984.go@gmail.com>
2020-04-05 15:49:36 +0200

    completato layout articoli

--- wp-content/themes/understrap/sidebar-templates/sidebar-right.php
+++ wp-content/themes/understrap/sidebar-templates/sidebar-right.php
@@ -17,10 +17,11 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 ?>
 
 <?php if ( 'both' === $sidebar_pos ) : ?>
-	<div class="col-md-3 widget-area py-8 px-3" id="right-sidebar" role="complementary">
+	<div class="col-md-3 widget-area py-8 px-3 sticky-top" id="right-sidebar" role="complementary">
 <?php else : ?>
 	<div class="col-md-4 widget-area py-8 px-3" id="right-sidebar" role="complementary">
+		<div class="sticky-top pt-5">
 <?php endif; ?>
 <?php dynamic_sidebar( 'right-sidebar' ); ?>
-
+		</div>
 </div><!-- #right-sidebar -->
