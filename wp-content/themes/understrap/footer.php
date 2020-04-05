62f329f795870c8f2887a15678d9c8a4b1a594a7
Francesco Matacotti <francesco1984.go@gmail.com>
2020-04-05 18:44:27 +0200

    aggiornato link definitivi footer

--- wp-content/themes/understrap/footer.php
+++ wp-content/themes/understrap/footer.php
@@ -25,17 +25,17 @@ $container = get_theme_mod( 'understrap_container_type' );
       <div class="col-12 col-md-4 footer-col text-center">
         <h6 class="pb-3">CONTATTI</h6>
         <ul class="pl-0">
-          <li style="font-size: 30px;" class="list-inline-item mr-5"><a href="#"><i class="fas fa-phone-alt"></i></a></li>
-          <li style="font-size: 30px;" class="list-inline-item"><a href="#"><i class="fas fa-envelope"></i></a></li>
+          <li style="font-size: 30px;" class="list-inline-item mr-5"><a href="tel:3342158907"><i class="fas fa-phone-alt"></i></a></li>
+          <li style="font-size: 30px;" class="list-inline-item"><a href="mailto:info@valeriomarrucci.it"><i class="fas fa-envelope"></i></a></li>
         </ul>
       </div>
 
       <div class="col-12 col-md-4 footer-col text-center">
         <h6 class="pb-3">SOCIAL</h6>
         <ul class="list-inline pl-0">
-          <li style="font-size: 30px;" class="list-inline-item mr-5"><a href="#"><i class="fab fa-instagram"></i></a></li>
-          <li style="font-size: 30px;" class="list-inline-item mr-5"><a href="#"><i class="fab fa-behance"></i></a></li>
-          <li style="font-size: 30px;" class="list-inline-item"><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+          <li style="font-size: 30px;" class="list-inline-item mr-5"><a href="https://www.instagram.com/valerio_marr/"><i class="fab fa-instagram"></i></a></li>
+          <li style="font-size: 30px;" class="list-inline-item mr-5"><a href="https://www.behance.net/valeriom115999"><i class="fab fa-behance"></i></a></li>
+          <li style="font-size: 30px;" class="list-inline-item"><a href="https://www.facebook.com/mementolucemsemper"><i class="fab fa-facebook-square"></i></a></li>
         </ul>
       </div>
 
@@ -53,27 +53,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 </div><!-- #page we need this extra closing tag here -->
 
 <?php wp_footer(); ?>
-
-<!--Navbar Animation-->
-<script>
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function () {
-    var currentScrollpos = window.pageYOffset;
-
-      if (prevScrollpos > currentScrollpos) {
-        //document.getElementsByTagName("nav")[0].style.backgroundColor = 'red';
-      } 
-      else {
-        document.getElementsByTagName("nav")[0].style.backgroundColor = '#343a40';
-      }
-
-      if(currentScrollpos === 0){
-        document.getElementsByTagName("nav")[0].style.backgroundColor = 'rgba(255,255,255,0)';
-      }
-
-      prevScrollpos = currentScrollpos;
-    };
-</script>
 
 </body>
 
